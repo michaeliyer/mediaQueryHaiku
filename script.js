0101387078a4935b470665e64cae51117fb5bc41
Michael Iyer <your-email@example.com>
2025-02-28 14:39:20 -0500

    Fuzzy, in and out, and opacity and more

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const anotherThing = "Man";
 console.log(anotherThing);
 const yetAnotherThing = "Good job";
 console.log(yetAnotherThing);
+
+const blueLink = document.querySelector(".line-1a");
+blueLink.addEventListener("click", changeOpacity);
+
+function changeOpacity() {
+    document.body.innerText = "NICE WORK CALVINO";
+}
