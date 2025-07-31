427bf3118762d85ef50d0d821461a0c2b9814632
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-07-30 23:17:08 -0400

    Assay plus bs

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ console.log(moreThing);
 const anotherThing = "Man";
 console.log(anotherThing);
 const yetAnotherThing = "Good job";
-console.log(yetAnotherThing, "You suck Man. Got, um, that!");
+console.log(yetAnotherThing, "You, um, suck Man. Got, um, that!");
 
 const blueLink = document.querySelector(".line-1a");
 blueLink.addEventListener("click", changeOpacity);
