843d4a51dd8d0838a44e56e994c6cf9329b4781b
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-08-04 23:35:12 -0400

    Rigid plus bs, again

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ console.log(moreThing);
 const anotherThing = "Man";
 console.log(anotherThing);
 const yetAnotherThing = "Good job";
-console.log(yetAnotherThing, "You, um, suck, um, Man. Got, um, that?");
+console.log(yetAnotherThing, "You, um, suck, um, Man. Got, um, that?!?");
 
 const blueLink = document.querySelector(".line-1a");
 blueLink.addEventListener("click", changeOpacity);
