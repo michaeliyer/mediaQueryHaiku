750bd65414efab2f1bda876b041b2c7d681f85b3
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-08-12 22:53:37 -0400

    Nomad plus bs

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ console.log(moreThing);
 const anotherThing = "Man";
 console.log(anotherThing);
 const yetAnotherThing = "Good job";
-console.log(yetAnotherThing, "You, um, suck a lot, um, Man. Um, Got, um, that, anal boy?!?");
+console.log(yetAnotherThing, "You, um, suck a lot, um, Man. Um, Got, um, that, anal, er, boy?!?");
 
 const blueLink = document.querySelector(".line-1a");
 blueLink.addEventListener("click", changeOpacity);
