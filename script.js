8ef8d571bc052872d3d3824955cbc1411d742567
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-09-02 23:21:18 -0400

    Might plus bs, more man

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const blueLink = document.querySelector(".line-1a");
 blueLink.addEventListener("click", changeOpacity);
 
 function changeOpacity() {
-    document.body.innerText = "NICE WORK Old, um, CALVINO, er, Asshole, ahem, gaa, um, er, ah, er, that, er, ah, you, ah, are, see!";
+    document.body.innerText = "NICE WORK Old, um, CALVINO, er, Asshole, ahem, gaaaq, um, er, ah, er, that, er, ah, you, ah, are, see!";
     console.log("Bite me wire, I declare!");
 }
 
