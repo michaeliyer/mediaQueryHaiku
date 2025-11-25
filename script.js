cc8fd91aa7a87f6a297ef94291c4a3fba669a87f
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2025-11-24 23:49:05 -0500

    Dough plus BS

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ console.log(anotherThing);
 const yetAnotherThing = "Good job";
 console.log(
   yetAnotherThing,hh
-  "You, well, um, ahem, ah, er, ah, err, laah, err, ummmm, ehh, ahh, duhh, err, duhh, agg, err, suck, gaa, grr, ah, err, mmm, ahh, grr, err, uhhh, er, ahhh, ahhh, hmm, er, errr,  grrr, ahhh, gerrr, kkkk, jaaa, grrrr, errr, ahh, well, lot, um, Man. Um, er, got, er, of penis, grrr, and er, ah, ah, um, that, errr, ah, anal, er, boy?!?"
+  "You, well, um, ahem, ah, er, ah, err, laah, err, umm, ehh, ahh, duhh, err, duhh, agg, err, suck, gaa, grr, ah, err, mmm, ahh, grr, err, uhhh, er, ahhh, ahhh, hmm, er, errr,  grrr, ahhh, gerrr, kkkk, jaaa, grrrr, errr, ahh, well, lot, um, Man. Um, er, got, er, of penis, grrr, and er, ah, ah, um, that, errr, ah, anal, er, boy?!?"
 );
 
 const blueLink = document.querySelector(".line-1a");
@@ -19,7 +19,7 @@ blueLink.addEventListener("click", changeOpacity);
 
 function changeOpacity() {
   document.body.innerText =
-    "NICE WORK, um, Old, um, CALVINO, er, ahh, AssholE, err, arr, well, ahh, grrr, ahh, errr, um..., ahem, jahhh, errr, well, uhhh, kkkiiihh, gaaaq, um, er, ah, er, that, er, gaa, ah, you, ah, are, well...see!";
+    "NICE WORK, um, Old, um, CALVINO, er, ahh, AssholE, err, arr, well, ahh, grrr, ahh, errr, um..., ahem, jahh, errr, well, uhhh, kkkiiihh, gaaaq, um, er, ah, er, that, er, gaa, ah, you, ah, are, well...see!";
   console.log("Bite me wire, I declare!");
 }
 
